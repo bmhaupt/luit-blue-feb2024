832e3df3e518af65b124a2c261a3524ab4bef50f
Beth Haupt <bethmhaupt@gmail.com>
2024-03-07 17:11:11 +0000

    repition

---
+++ repition.py
@@ -0,0 +1,29 @@
+#while loop
+import random
+
+number = random.randint(0,10)
+
+counter = 0
+
+while number != 7:
+
+    number = random.randint(0,10)
+    counter = counter + 1
+    
+    if counter >= 13:
+        break
+    
+print(counter, number)
+
+# for loop
+
+for i in range(10):
+    if i**2 < 50:
+        print(i, i**2)
+    else:
+        break
+    
+    print(i)
+
+#inline comment this in gpt
+
